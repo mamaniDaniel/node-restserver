42fd53e248e4652096441cb5fdfe7a710b5f0de2
mamaniDaniel <dani.oscar.257@gmail.com>
2019-11-03 23:49:17 -0300

    OK: agregado token y seed de autenticacion

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,26 @@ process.env.PORT = process.env.PORT || 3000;
 // Entorno
 // ==========================================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+//A: NODE_ENV la crea heroku, me ayuda a saber si estoy en produccion o desarrollo
 
+// =========================================
+// Vencimiento del token
+// ==========================================
+//60 segundos
+//60 minutos
+//24 horas
+//30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// =========================================
+// Seed de autenticacion
+// ==========================================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+//heroku config (ver variable creadas)
+//heroku config:set nombreVariable="valor" 
+//puedo crear una seed en horoku para evitar que la semilla este visible
+//en github
 
 // =========================================
 // Base de datos
